0ddf20cd5d3819d443fd02003d8fd9f1e7c9f75f
Yasser Aloqoul <yasser.aqoul@cloudvests.com>
2025-08-12 13:56:42 +0300

    creating a responsive layout

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Button, Grid,Show, GridItem } from '@chakra-ui/react'
+
+
+function App() {
+  const hello ="dasd";
+  console.log(hello)
+  return (<Grid templateAreas={{
+    base: `"nav" "main"`,
+    lg:`"nav nav" "aside main"`
+  }}>
+    
+    <GridItem area='nav' bg='coral'>Nav</GridItem>
+    <Show above='lg'>
+    <GridItem area='aside' bg='gold'>Aside</GridItem>
+    </Show>
+    <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+  </Grid>);
+
+}
+
+export default App
