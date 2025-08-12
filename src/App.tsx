62a87f43878e932a0b88d7a91bd940fe09d89f0c
Yasser Aloqoul <yasser.aqoul@cloudvests.com>
2025-08-12 14:48:28 +0300

    added a NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import { Button, Grid,Show, GridItem } from '@chakra-ui/react'
+import NavBar from './components/NavBar';
 
 
 function App() {
-  const hello ="dasd";
-  console.log(hello)
   return (<Grid templateAreas={{
     base: `"nav" "main"`,
     lg:`"nav nav" "aside main"`
   }}>
     
-    <GridItem area='nav' bg='coral'>Nav</GridItem>
+    <GridItem area='nav' >
+      <NavBar/>
+    </GridItem>
+
     <Show above='lg'>
-    <GridItem area='aside' bg='gold'>Aside</GridItem>
+    <GridItem area='aside' bg='gold'>
+      Aside
+    </GridItem>
     </Show>
-    <GridItem area='main' bg='dodgerblue'>Main</GridItem>
-  </Grid>);
+
+    <GridItem area='main' bg='dodgerblue'>
+      Main
+    </GridItem>
+  </Grid>
+  );
 
 }
 
